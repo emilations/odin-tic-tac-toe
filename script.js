f61eafb7222988aa6169fa76a5f0be8a3da04b49
Emile Mouannes <emile.mouannes@outlook.com>
2022-07-29 16:34:28 -0400

    Add name input with a start functionality

--- script.js
+++ script.js
@@ -22,7 +22,11 @@ let gameboard = (function () {
 let game = (function () {
   let players = [];
   let currentPlayerIndex = 0;
-  let start = function () {};
+  let start = function () {
+    interface.getNames();
+    interface.displayShow("board");
+    
+  };
   let setPlayer = function (name, token) {
     players.push({ name, token });
   };
@@ -35,7 +39,7 @@ let game = (function () {
     gameboard.update(position, token);
     interface.displayUpdate(position, token);
     currentPlayerIndex = currentPlayerIndex == 0 ? 1 : 0;
-    interface.removeListener(position)
+    interface.removeListener(position);
   };
 
   return {
@@ -48,19 +52,58 @@ let game = (function () {
 
 // interface controller module
 let interface = (function () {
+  let intro;
+  let board;
+  let outro;
   let grid;
+  let startButton;
+
   let cacheDOM = function () {
+    intro = document.querySelector(".intro");
+    board = document.querySelector(".board");
+    outro = document.querySelector(".outro");
     grid = document.querySelectorAll(".grid");
+    startButton = document.querySelector(".start");
   };
   let addListener = function () {
     grid.forEach((elem) => elem.addEventListener("click", game.round));
+    startButton.addEventListener("click", game.start);
   };
   let displayUpdate = function (position, content) {
     grid[position].textContent = content;
   };
-  let removeListener = function (position){
+  let removeListener = function (position) {
     grid[position].removeEventListener("click", game.round);
-  }
+  };
+  let displayShow = function (ui) {
+    if (ui == "intro") {
+      intro.classList.remove("hidden");
+      board.classList.add("hidden");
+      outro.classList.add("hidden");
+    } else if (ui == "board") {
+      board.classList.remove("hidden");
+      intro.classList.add("hidden");
+      outro.classList.add("hidden");
+    } else if (ui == "outro") {
+      outro.classList.remove("hidden");
+      board.classList.add("hidden");
+      intro.classList.add("hidden");
+    }
+  };
+  let getNames = function () {
+    let playerOneName = document.querySelector("#playerOne").value;
+    let playerTwoName = document.querySelector("#playerTwo").value;
+    if (!playerOneName) {
+      playerOneName = "Player One"
+    }
+    if (!playerTwoName) {
+      playerTwoName = "Player Two"
+    }
+
+    game.setPlayer(playerOneName, "X");
+    game.setPlayer(playerTwoName, "O");
+  };
+
   cacheDOM();
   addListener();
 
@@ -69,6 +112,8 @@ let interface = (function () {
     addListener,
     displayUpdate,
     removeListener,
+    displayShow,
+    getNames,
   };
 })();
 
@@ -76,6 +121,3 @@ let interface = (function () {
 function player(name, token) {
   return { name, token };
 }
-
-game.setPlayer("Sofia", "O");
-game.setPlayer("Emile", "X");
