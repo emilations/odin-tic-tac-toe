24b4fb9fcf5516332176958f97ee33b0be6636d9
Emile Mouannes <emile.mouannes@outlook.com>
2022-07-29 13:23:19 -0400

    Add hidden elements and start UI

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
-let gameboard = {
-  memory: ["", "", "", "", "", "", "", "", ""],
-  modify: function (position, content) {
-    this.memory[position] = content;
-  },
-  clear: function () {
-    this.memory = ["", "", "", "", "", "", "", "", ""];
-  },
-}
+// gameboard memory module
+let gameboard = (function () {
+  let memory = ["", "", "", "", "", "", "", "", ""];
+  let read = function () {
+    return [...memory];
+  };
+  let update = function (position, content) {
+    memory[position] = content;
+  };
+  let reset = function () {
+    memory = ["", "", "", "", "", "", "", "", ""];
+  };
 
-let gameInterface = {
-  init: function () {
-    this.cacheDOM();
-    this.addListener();
-    this.displayRefresh();
-  },
-  cacheDOM: function () {
-    this.boxes = document.querySelectorAll(".box");
-  },
-  addListener: function () {
-    this.boxes.forEach((box) => box.addEventListener("click", game.round));
-  },
-  removeListener: function() {
+  return {
+    read,
+    update,
+    reset,
+  };
+})();
 
-  },
-  displayRefresh: function () {
-    for (let position in this.boxes)
-      this.boxes[position].innerHTML = gameboard.memory[position];
-  },
-};
+// game module
+let game = (function () {
+  let players = [];
+  let currentPlayerIndex = 0;
+  let start = function () {};
+  let setPlayer = function (name, token) {
+    players.push({ name, token });
+  };
+  let readPlayer = function () {
+    console.log(players);
+  };
+  let round = function (e) {
+    let position = e.target.dataset.index;
+    let token = players[currentPlayerIndex].token;
+    gameboard.update(position, token);
+    interface.displayUpdate(position, token);
+    currentPlayerIndex = currentPlayerIndex == 0 ? 1 : 0;
+    interface.removeListener(position)
+  };
 
-let game = {
-  playerTurn: "",
-  winner: "",
-  init: function () {
-    gameboard.clear()
-    gameInterface.init();
-  },
-  round: function(e) {
-    let index = e.target.dataset.index;
-    gameboard.modify(index, playerOne.mark);
-    gameInterface.displayRefresh();
-  },
-  setSelection: function(e) {
-    playerOne.mark = e.target.value;
+  return {
+    start,
+    setPlayer,
+    readPlayer,
+    round,
+  };
+})();
+
+// interface controller module
+let interface = (function () {
+  let grid;
+  let cacheDOM = function () {
+    grid = document.querySelectorAll(".grid");
+  };
+  let addListener = function () {
+    grid.forEach((elem) => elem.addEventListener("click", game.round));
+  };
+  let displayUpdate = function (position, content) {
+    grid[position].textContent = content;
+  };
+  let removeListener = function (position){
+    grid[position].removeEventListener("click", game.round);
   }
-};
+  cacheDOM();
+  addListener();
 
-game.init();
-let playerOne = player("X", "Yolo")
-
+  return {
+    cacheDOM,
+    addListener,
+    displayUpdate,
+    removeListener,
+  };
+})();
 
 // factory for player
-function player (mark, name) {
-  return {name,
-          mark,
-  }
+function player(name, token) {
+  return { name, token };
 }
+
+game.setPlayer("Sofia", "O");
+game.setPlayer("Emile", "X");
